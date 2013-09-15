c39759aaafcde4a037ced001bc6e80f27d506187
unknown <nick.shahan@gmail.com>
2013-09-15 01:00:05 -0700

    now working with filetypes

--- organizer.py
+++ organizer.py
@@ -1,15 +1,18 @@
 import sys
 import os
 import glob
+import datetime
+import shutil
 
 
 #count args
-if len(sys.argv) != 2:
-	print("ERROR: Organizer requires one path as an argument.")
+if len(sys.argv) != 3:
+	print("ERROR: Organizer requires two arguments: A path and a file type.")
 	exit(1)
 
 #verify folder exists
 path = sys.argv[1]
+filetype = sys.argv[2]
 
 if not os.path.exists(path):
 	print("Path", path, "does not exist.")
@@ -17,7 +20,16 @@ if not os.path.exists(path):
 
 #read all jpg files in the directory
 os.chdir(path)
-files = glob.glob("*.jpg")
+files = glob.glob("*." + filetype)
 
-for f in files: 
-	print(f)
+for f in files:
+	time = os.path.getmtime(f) 
+	date = str(datetime.date.fromtimestamp(time))
+	
+	#create new folder if it does not already exist
+	if not os.path.exists(date):
+		os.makedirs(date)
+
+	#move file to new folder
+	dst = date + "\\" + f
+	shutil.move(f, dst)	
